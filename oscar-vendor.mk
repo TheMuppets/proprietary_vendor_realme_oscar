4a0f94eb4da07040473aa774473c0b1350856695
Vivekachooz <vivekachooz@gmail.com>
2025-03-06 09:19:19 +0530

    oscar: Update from RMX3471_14.0.0.1900(EX01)
    Change-Id: I38e42dc56a4f2f5a8975702f5f77fc04b064624c

--- oscar-vendor.mk
+++ oscar-vendor.mk
@@ -1178,7 +1178,6 @@ PRODUCT_PACKAGES += \
     libmpbase \
     libop-ai-beauty-body-detection \
     libop-ai-beauty-faceretouch-cn \
-    liboplusblur_capture_api \
     libormshalclient \
     libosenseaidlhalclient \
     libosensehalclient \
